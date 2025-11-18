58dc4915947d821fa68cb2169133e7454cfd02bb
ngvp-nube <ni.villagrap@duocuc.cl>
2025-11-18 17:09:11 -0300

    uppdate

--- myProyectoValdiviano/settings.py
+++ myProyectoValdiviano/settings.py
@@ -35,8 +35,9 @@ ALLOWED_HOSTS = ['*,192.168.1.102']
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://192.168.1.100:4200",  # Frontend Angular en el servidor (si está corriendo ahí)
-    "http://192.168.1.101:4200",  # Frontend en otro PC cliente (en este caso, IP 192.168.1.101)
+    "http://192.168.1.103:4200",  # Frontend Angular en el servidor (si está corriendo ahí)
+    "http://192.168.1.103:4200",
+    "http://192.168.1.103:4200",  # Frontend en otro PC cliente (en este caso, IP 192.168.1.101)
     "http://192.168.1.10:4200",  # Frontend en otro PC cliente (en este caso, IP 192.168.1.101)
     "http://localhost:8080",      # Para pruebas locales (si accedes desde localhost)
     "http://127.0.0.1:8080",      # También localhost, por si acaso
